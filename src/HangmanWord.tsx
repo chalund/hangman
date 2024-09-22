c47c9a15e7ff2fb13f2d7018c09fd50222b17e3b
chalund <114383740+chalund@users.noreply.github.com>
2024-09-22 14:47:45 +0200

    adds hangman word

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,30 @@
 export function HangmanWord() {
-  return <div></div>;
+  const word = 'test';
+  const guessedLetters = ['t', 's', ];
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25rem',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}
+    >
+      {word.split('').map((letter, index) => (
+        <span style={{ borderBottom: '.1em solid black' }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? 'visible'
+                : 'hidden',
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
